eff1066f669d84e89721dd57947bf48ecbe87e61
Snaggly <oemshani@yahoo.com>
2020-04-02 18:44:15 +0200

    Type lookup fix
    Type lookup in Reader was messy. Cleaned it up by hopping from type to type

--- src/snaggly/Reader.java
+++ src/snaggly/Reader.java
@@ -5,22 +5,39 @@ import java.io.IOException;
 public class Reader {
 	
 	public static <T extends IType> T lookupType(String inputFile, T type) throws IOException, UnexpectedFileException{
-		boolean check = false;
 		IntInputFileStream stream = new IntInputFileStream(inputFile);
-		char[] typeCheck = type.getType().toCharArray();
+		String typeCheck = type.getType();
 		
+		if (stream.readInt() != 1297239878) {
+			stream.close();
+			throw new UnexpectedFileException("FORM not found!");
+		}
+
 		//Seek for the offset for given type in file
-		while(stream.available() > 0 && !check) {
-			for (byte b = 0; b<4; b++) {
-				check = stream.read() == typeCheck[b];
+		Logger.print("Looking for the container...");
+		int currentPos = 0;
+		int maxSize = stream.readInt();
+		char[] buffer = new char[typeCheck.length()];
+				
+		while (currentPos < maxSize) {
+			for (byte b = 0; b<typeCheck.length(); b++) {
+				buffer[b] = (char) stream.read();
 			}
+			if (String.copyValueOf(buffer).equals(typeCheck)) {
+				break;
+			}
+			int r = stream.readInt();
+			currentPos += typeCheck.length() + r;
+			stream.skip(r);
 		}
 		
 		//Throw exception if type wasn't found
-		if (stream.available() <= 0) {
+		if (currentPos >= maxSize || stream.available() <= 0) {
 			stream.close();
 			throw new UnexpectedFileException();
 		}
+		
+		Logger.print("Found " + typeCheck + " @ " + currentPos);
 		
 		//Decoding header data
 		type.setChunkSize(stream.readInt());
--- src/snaggly/UnexpectedFileException.java
+++ src/snaggly/UnexpectedFileException.java
@@ -2,5 +2,11 @@ package snaggly;
 
 @SuppressWarnings("serial")
 public class UnexpectedFileException extends Exception {
-
+	public UnexpectedFileException() {
+		super();
+	}
+	
+	public UnexpectedFileException(String message) {
+		super(message);
+	}
 }
